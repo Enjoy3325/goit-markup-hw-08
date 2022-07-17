8e4bb9c40bd8cc90145b799a90bb3fc9023c68a5
Enjoy3325 <enjoy3325@gmail.com>
2022-07-17 19:22:48 +0300

    prepush

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  function handleCloseModal(e) { 
+    if(e.key === "Escape") {
+      refs.modal.classList.add('is-hidden');
+    }
+  }
+  
+  window.addEventListener('keydown', handleCloseModal);
+})();
+
